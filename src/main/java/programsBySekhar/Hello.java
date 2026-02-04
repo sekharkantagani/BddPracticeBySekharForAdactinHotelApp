e7ecdd11e846e57f1d9078f417e5ba71c18084b8
sekharkantagani <chandrasekhar9408@gmail.com>
2026-02-04 08:14:29 +0530

    second changes

---
+++ src/main/java/programsBySekhar/Hello.java
@@ -0,0 +1,12 @@
+package programsBySekhar;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Skehar");
+
+	}
+
+}
